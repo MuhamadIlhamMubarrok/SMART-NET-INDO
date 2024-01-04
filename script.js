5a6cb90b41aa436245ee8803d69561b442084eac
MuhamadIlhamMubarrok <milhammubarrok@gmail.com>
2024-01-04 08:40:18 +0700

    second

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var mobileMenu = document.getElementById('mobileMenu');
-  var menuButton = document.getElementById('menuButton');
-  var menuImage = document.getElementById('menuImage');
+document.addEventListener("DOMContentLoaded", function () {
+  var mobileMenu = document.getElementById("mobileMenu");
+  var menuButton = document.getElementById("menuButton");
+  var menuImage = document.getElementById("menuImage");
 
-  menuButton.addEventListener('click', function () {
-    mobileMenu.classList.toggle('hidden');
-    var isMenuHidden = mobileMenu.classList.contains('hidden');
-    menuImage.src = isMenuHidden
-      ? './assets/menu.png'
-      : './assets/exit-menu.png';
+  menuButton.addEventListener("click", function () {
+    mobileMenu.classList.toggle("hidden");
+    var isMenuHidden = mobileMenu.classList.contains("hidden");
+    menuImage.src = isMenuHidden ? "./assets/menu.png" : "./assets/exit-menu.png";
   });
 
-  var submenuToggles = document.querySelectorAll('.submenu-toggle');
+  var submenuToggles = document.querySelectorAll(".submenu-toggle");
   submenuToggles.forEach(function (toggle) {
-    toggle.addEventListener('click', function () {
-      var submenuId = toggle.getAttribute('data-submenu');
-      var submenu = document.getElementById('submenu-' + submenuId);
-      submenu.classList.toggle('hidden');
+    toggle.addEventListener("click", function () {
+      var submenuId = toggle.getAttribute("data-submenu");
+      var submenu = document.getElementById("submenu-" + submenuId);
+      submenu.classList.toggle("hidden");
 
-      var arrowIcon = toggle.querySelector('ion-icon');
-      if (submenu.classList.contains('hidden')) {
-        arrowIcon.setAttribute('name', 'arrow-dropdown');
+      var arrowIcon = toggle.querySelector("ion-icon");
+      if (submenu.classList.contains("hidden")) {
+        arrowIcon.setAttribute("name", "arrow-dropdown");
       } else {
-        arrowIcon.setAttribute('name', 'arrow-dropup');
+        arrowIcon.setAttribute("name", "arrow-dropup");
       }
     });
   });
